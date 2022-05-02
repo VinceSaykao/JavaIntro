3236a453e99e5dacfdcc5f158398221cb1084335
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-05-02 14:41:11 -0500

    sout a reference type, date now

--- src/com.vincesaykao/Main.java
+++ src/com.vincesaykao/Main.java
@@ -1,11 +1,15 @@
-import java.io.Console;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
         byte age = 30;
         int viewsCount = 123_456_483;
         // console.log(age);
         Date now = new Date();
+        System.out.println(now);
     }
 }
+
+// primitive type, we define the data
+// reference type, we create undefined data. 
