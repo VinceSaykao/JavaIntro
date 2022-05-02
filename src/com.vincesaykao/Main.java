f4f843c9c229f4f13eda7be8a1b4ba6358a9c3c8
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-05-02 14:35:57 -0500

    added an object of an instance of a class

--- src/com.vincesaykao/Main.java
+++ src/com.vincesaykao/Main.java
@@ -1,9 +1,11 @@
-package come.codewithmosh;
+import java.io.Console;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        // byte age = 30;
-        // int viewsCount = 123_456_483;
+        byte age = 30;
+        int viewsCount = 123_456_483;
+        // console.log(age);
+        Date now = new Date();
     }
 }
