20ef31712c0135c679320cda2ca3b664eea13f05
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-05-02 16:00:23 -0500

    learned java methods

--- src/com.vincesaykao/Main.java
+++ src/com.vincesaykao/Main.java
@@ -1,15 +1,26 @@
 import java.util.Date;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         // System.out.println("Hello World");
-        byte age = 30;
-        int viewsCount = 123_456_483;
-        // console.log(age);
-        Date now = new Date();
-        System.out.println(now);
+        // byte age = 30;
+        // int viewsCount = 123_456_483;
+        // // console.log(age);
+        // Date now = new Date();
+        // System.out.println(now);
+        // Point point1 = new Point(1, 1);
+        // Point point2 = point1;
+        // point1.x = 2;
+        // System.out.println(point2);
+        String message = "Hello World" + "!!";
+        System.out.println(message.replace("!","~"));
     }
 }
 
-// primitive type, we define the data
-// reference type, we create undefined data. 
+// primitive type, we define the data, store number, characters, booleans
+// reference type, we create referenced data that has the address to that value data, store complex objects (date, mail messages)
+// methods: 
+// indexOf() = index of start character 
+// replace() = replace target:, replacement: 
+
