f03983a45955c3cf938a1d47ab0001e63db4b75c
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-05-02 14:33:15 -0500

    cleaned files

--- src/com.vincesaykao/Main.java
+++ src/com.vincesaykao/Main.java
@@ -3,5 +3,7 @@ package come.codewithmosh;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // byte age = 30;
+        // int viewsCount = 123_456_483;
     }
 }
