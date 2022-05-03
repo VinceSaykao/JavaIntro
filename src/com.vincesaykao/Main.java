0880fc5d315f07d08c2f286002bcfc8f1f498104
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-05-03 11:43:05 -0500

    got tic tac toe to work!

--- src/com.vincesaykao/Main.java
+++ src/com.vincesaykao/Main.java
@@ -1,213 +1,91 @@
-import java.util.Arrays;
-import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.awt.*;
-import java.text.NumberFormat;
 
-public class Main {
-    public static void main(String[] args) {
-        // System.out.println("Hello World");
-        // byte age = 30;
-        // int viewsCount = 123_456_483;
-        // // console.log(age);
-        // Date now = new Date();
-        // System.out.println(now);
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static List<Pieces> plays = new ArrayList<>();
 
-        // Point point1 = new Point(1, 1);
-        // Point point2 = point1;
-        // point1.x = 2;
-        // System.out.println(point2);
+    public static void main(String[] args)
+    {
+        char player1 = 'X';
+        char player2 = 'Y';
+        Scanner in = new Scanner(System.in);
 
-        // String message = "Hello World" + "!!";
-        // System.out.println(message.replace("!","~"));
+        Board gameBoard = new Board(3, 3);
 
-        // int[] numbers = new int[5];
-        // numbers[0] = 1;
-        // numbers[1] = 2;
-        // System.out.println(Arrays.toString(numbers));
+        // making plays
+        char turn = player1;
+        boolean stop = false;
 
-        // int[] numbers = {2,3,4,1,2};
-        // System.out.println(numbers.length);
+        String winner = "";
+        gameBoard.printBoard();
+        while (!stop)
+        {
+            boolean goodPlay = false;
+            if (turn == player1)
+            {
+                // make sure a play is available
+                if (gameBoard.spaceAvailable())
+                {                
+                    // make a play
+                    do 
+                    {
+                        System.out.print("Please enter row col:" );
+                        int row = in.nextInt();
+                        int col = in.nextInt();
 
-        // int[][] numbers = {{1,2,3}, {3,2,3}};
-        // numbers[0][0] = 1;
-        // System.out.println(Arrays.deepToString(numbers));
+                        goodPlay = gameBoard.play(turn, row - 1, col - 1);
+                    } while (!goodPlay);
+                } else
+                {
+                    goodPlay = false;
+                }
+            } else
+            {
+                goodPlay = gameBoard.autoPlay(turn);
+            }
 
-        // final float PI = 3.14F;
+            if (goodPlay)
+            {
+                // autoplay successful. check for winner
+                if (gameBoard.haveAWinner())
+                {
+                    // we have a winner so stop playing
+                    winner = "Player " + turn + " is our winner!!!";
+                    stop = true;
+                } else
+                {
+                    // we don't have a winner so switch players
+                    turn = (turn == player1) ? player2 : player1;
+                }
+            } else
+            {
+                // a player could not make a play
+                winner = "We do not have a winner :-(";
+                stop = true;
+            }
 
-        // int x = 1;
-        // x += 1;
-        // System.out.println(x);
+            gameBoard.printBoard();
+            
+            try
+            {
+                Thread.sleep(1000);
+            } catch (Exception e)
+            {
+                
+            }
+        }
 
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Name: ");
-        // String name = scanner.nextLine().trim();
-        // System.out.println("You are" + " " + name);
-
-
-        // Mortage Calculator START *********
-
-        // final byte MONTHS_IN_YEAR = 12;
-        // final byte PERCENT = 100;
-
-        // Scanner scanner = new Scanner(System.in);
-
-        // System.out.println("Principal: ");
-        // int principal = scanner.nextInt();
-
-        // System.out.print("Annual Interest Rate: ");
-        // float annualInterest = scanner.nextFloat();
-        // float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        // System.out.println("Period (Years): ");
-        // byte years = scanner.nextByte();
-        // int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        // double mortage = principal
-        //         * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-        //         / (Math.pow(1 + monthlyInterest, numberOfPayments));
-        // String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
-        // System.out.println("Mortage: " + mortageFormatted);
-
-        // Mortage Calculator END *********
-
-        // Logical Operator
-
-        // int temperature = 12;
-        // boolean isWarm = temperature > 20 && temperature < 30;
-        // System.out.println(isWarm);
-
-        // int temp = 1;
-        // if (temp > 30) {
-        //     System.out.println("It's a hot day, drink water!");
-        // } else if (temp > 20 && temp <= 30) {
-        //     System.out.println("Beautiful day");
-        // } else {
-        //     System.out.println("Brrr cold");
-        // };
-
-        // int income = 120_000;
-        // // boolean hasHighIncome = (income > 100_000);
-        // String className = (income > 100_000 ? "First" : "Economy");
-        // System.out.println(className);
-
-        // String role = "stuff";
-
-        // switch (role) {
-        //     case "admin": 
-        //         System.out.println("You are an admin");
-        //     break;
-
-        //     case "mod":
-        //         System.out.println("You are a mod");
-        //     break;
-
-        //     default:
-        //     System.out.println("You are a guest");
-        // }
-
-        // System.out.println(role);
-
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.print("Number: ");
-        // int number = scanner.nextInt();
-
-        // if ( number % 5 == 0 && number % 3 == 0)
-        //     System.out.println("Fizz Buzz");
-        // else if (number % 3 == 0)
-        //     System.out.println("Buzz");
-        // else if (number % 5 == 0)
-        //     System.out.println("Fizz");
-
-        // for (int i=5; i>0; i--)
-        //     System.out.println("Hello World" + i);
-
-        // String input = "";
-        // Scanner scanner = new Scanner(System.in);
-
-        // while(true) {
-        //     System.out.println("Input: " + input);
-        //     input = scanner.next().toLowerCase();
-        //     // System.out.println(input);
-        //     if(input.equals("pass"))
-        //     continue;
-        //     if(input.equals("quit"))
-        //     break;
-        // }
-
-        // do {
-
-        // } while (!input.equals("quit"));
-        // System.out.println("Input: ");
-        // input = scanner.next().toLowerCase();
-        // System.out.println(input);
-
-        // int access_level = 1;
-
-        // switch(access_level) {
-
-        //     case 3: 
-        //     System.out.println("you 3");
-        //     break;
-        //     case 2:
-        //     System.out.println("you 2");
-        //     default:
-        //     System.out.println("none");
-        // }
-
-        // String[] fruits = {"apple","mango","orange"};
-        // for (int i=0;i<fruits.length;i++) {
-        //     System.out.println(fruits[i]);
-        // }; // end of for loop
-        // for(String fruit : fruits)
-        // System.out.println(fruit);
-
-    //     final byte MONTHS_IN_YEAR = 12;
-    //     final byte PERCENT = 100;
-
-    //     int principal = 0;
-    //     float monthlyInterest = 0;
-    //     int numberOfPayments = 0;
-    //     Scanner scanner = new Scanner(System.in);
-
-    //     while(true) {
-    //         System.out.println("Principal: ");
-    //         int principal = scanner.nextInt();
-    //         if (principal >= 1000 && principal <= 1_000_000 )
-    //             break;
-    //         System.out.println("enter value between 1000 and 1000000");
-    //     }
-
-    //     while(true) {
-    //         System.out.println("annual interest rate: ");
-    //         float annualInterest = scanner.nextFloat();
-    //         if (annualInterest >= 1 && annualInterest <= 30)
-    //             break;
-    //         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR
-    
-
-    //     System.out.println("period (years): ");
-    //     byte years = scanner.nextByte();
-    //     if ( years >= 1 && year <= 30) {
-    //         int numberOfPayments = years * MONTHS_IN_YEAR;
-    //         break;
-
-    //     }
-    //     System.out.println("enter value between 1 and 30");
-    // }
-    //     double mortage = principal
-    //             * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-    //             / (Math.pow(1 + monthlyInterest, numberOfPayments));
-    //     String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
-    //     System.out.println("Mortage: " + mortageFormatted);
+        gameBoard.printBoard(winner);
         
-        
+        System.out.println("\n*** Plays Made ***");
+        plays.forEach(x -> System.out.println(x));
     }
 }
-
-// primitive type, we define the data, store number, characters, booleans
-// reference type, we create referenced data that has the address to that value
-// data, store complex objects (date, mail messages)
-// methods:
-// indexOf() = index of start character
-// replace() = replace target:, replacement:
--- src/com.vincesaykao/Player.java
+++ src/com.vincesaykao/Player.java
@@ -1,12 +1,21 @@
-public class Player {
-    private final char name;
 
-    public Player(char name) {
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private final char name;
+    
+    public Player(char name)
+    {
         this.name = name;
     }
-
-    public char getName() {
+    
+    public char getName()
+    {
         return name;
     }
-    
 }
